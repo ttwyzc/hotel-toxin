d861b7bbdd49b627cdfa0731cf031c2af5863566
Kirill <ttwyzc@gmail.com>
2020-01-10 13:41:15 +0700

    block of colors on colos-n-types page

--- src/entry.js
+++ src/entry.js
@@ -2,3 +2,4 @@
  import './style/global.scss';
  import './components/header-minimal/header-minimal';
  import './components/container/container.js';
+ import './components/color-block/color-block.js'

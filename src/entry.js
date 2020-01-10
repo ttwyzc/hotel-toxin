5b7dc752480d952595d05955a7e4b6ef4b2f1444
Kirill <ttwyzc@gmail.com>
2020-01-10 14:34:54 +0700

    text-type on page colors-n-types

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,6 @@
  import 'normalize.css';
  import './style/global.scss';
  import './components/header-minimal/header-minimal';
- import './components/container/container.js';
- import './components/color-block/color-block.js'
+ import './components/container/container';
+ import './components/color-block/color-block'
+ import './components/text-type/text-type';

5d62dfc60de6dc632c60fd75f067fab47a881236
Kirill <ttwyzc@gmail.com>
2020-01-10 12:14:53 +0700

    header for ui-kit pages

---
+++ src/components/header-minimal/header-minimal.js
@@ -0,0 +1,1 @@
+import './header-minimal.scss';
--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,4 @@
  import 'normalize.css';
  import './style/global.scss';
+ import './components/header-minimal/header-minimal';
+ import './components/container/container.js';

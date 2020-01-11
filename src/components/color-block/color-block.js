e2a586fe34fcd3efb7bb105f91486ee5ca14d188
Kirill <ttwyzc@gmail.com>
2020-01-11 19:06:33 +0700

    from the begining

--- src/components/color-block/color-block.js
+++
@@ -1,1 +0,0 @@
-import './color-block.scss'
--- src/entry.js
+++
@@ -1,6 +0,0 @@
- import 'normalize.css';
- import './style/global.scss';
- import './components/header-minimal/header-minimal';
- import './components/container/container';
- import './components/color-block/color-block'
- import './components/text-type/text-type';

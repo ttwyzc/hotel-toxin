112aa2f2cf7e0930ed2c83e3cbc00b6044088896
Kirill <ttwyzc@gmail.com>
2020-01-24 23:58:57 +0700

    recursive parcel.js imports of components

--- src/entry.js
+++ src/entry.js
@@ -1,8 +1,3 @@
 import 'normalize.css';
 import './style/global.scss';
-import './components/header-min/header-min';
-import './components/wrapper/wrapper';
-import './components/colors/colors';
-import './components/text-type/text-type';
-import './components/button/button';
-import './components/input/input';
+import './components/*/*.js';
